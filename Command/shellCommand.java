3a04dc6cc95f4b369634e9d3b5393b38cbe50deb
Silas <hulingyu476@126.com>
2019-12-09 17:31:51 +0800

    Create shellCommand.java
    App call shell command

---
+++ Command/shellCommand.java
@@ -0,0 +1,71 @@
+/**
+ * Android shell 命令执行器，支持无限个命令串型执行（需要有root权限！！）
+ * <p>
+ * <p>
+ * HOW TO USE?
+ * Example:修改开机启动动画。把/sdcard/Download目录下的bootanimation.zip文件拷贝到
+ * /system/media目录下并修改bootanimation.zip的权限为777。
+ * <p>
+ * <pre>
+ *      int result = new ShellCommandExecutor()
+ *                  .addCommand("mount -o remount,rw /system")
+ *                  .addCommand("cp /sdcard/Download/bootanimation.zip /system/media")
+ *                  .addCommand("cd /system/media")
+ *                  .addCommand("chmod 777 bootanimation.zip")
+ *                  .execute();
+ * <pre/>
+ *
+ * @author silas.
+ */
+ 
+public class ShellCommandExecutor {
+    private static final String TAG = "ShellCommandExecutor";
+ 
+    private StringBuilder mCommands;
+ 
+    public ShellCommandExecutor() {
+        mCommands = new StringBuilder();
+    }
+ 
+    public int execute() {
+        return execute(mCommands.toString());
+    }
+ 
+    public ShellCommandExecutor addCommand(String cmd) {
+        if (TextUtils.isEmpty(cmd)) {
+            throw new IllegalArgumentException("command can not be null.");
+        }
+        mCommands.append(cmd);
+        mCommands.append("\n");
+        return this;
+    }
+ 
+    private static int execute(String command) {
+        int result = -1;
+        DataOutputStream dos = null;
+        try {
+            Process p = Runtime.getRuntime().exec("su");
+            dos = new DataOutputStream(p.getOutputStream());
+            Log.i(TAG, command);
+            dos.writeBytes(command + "\n");
+            dos.flush();
+            dos.writeBytes("exit\n");
+            dos.flush();
+            p.waitFor();
+            result = p.exitValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+ 
+ 
+}
